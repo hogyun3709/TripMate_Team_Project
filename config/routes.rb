de660a9c0333ce16170d53cd873ef1df0aafd633
hogyun3709 <hogyu3709@gmail.com>
2017-08-16 12:06:12 +1000

    posting draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
   devise_for :users
   get 'home/index'
+  get 'posts/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts
 end
