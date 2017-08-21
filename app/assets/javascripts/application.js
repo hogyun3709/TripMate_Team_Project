b83a39c250c57a78b28b7dfd22a00a3c361eae71
hogyun3709 <hogyu3709@gmail.com>
2017-08-22 00:18:31 +1000

    Jquery scroll

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,26 +16,30 @@
 //= require_tree .
 
 $(function() {
-  $('.counter').each(function() {
-  var $this = $(this),
-      countTo = $this.attr('data-count');
+    $(window).scroll(function() {
+        $('.counter').each(function() {
+            var $this = $(this),
+                countTo = $this.attr('data-count');
 
-  $({ countNum: $this.text()}).animate({
-    countNum: countTo
-  },
+            $({
+                countNum: $this.text()
+            }).animate({
+                    countNum: countTo
+                },
 
-  {
+                {
 
-    duration: 8000,
-    easing:'linear',
-    step: function() {
-      $this.text(Math.floor(this.countNum));
-    },
-    complete: function() {
-      $this.text(this.countNum);
-      //alert('finished');
-    }
+                    duration: 8000,
+                    easing: 'linear',
+                    step: function() {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function() {
+                        $this.text(this.countNum);
+                        //alert('finished');
+                    }
 
-  });
-  });
+                });
+        });
+    });
 });
