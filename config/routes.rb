e2f59851838e6ae6041c77b6795491061135f082
go555666 <jhg043@uowmail.edu.au>
2017-08-25 15:56:08 +1000

    making_recommand_place

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   get 'posts/index'
+  get 'recommand/index'
+  get 'home/recommand'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
