69a0829bf6fe2b6c6c08aeb4e96c1567e6b1cf77
hogyun3709 <hogyu3709@gmail.com>
2017-08-26 04:20:09 +1000

    before_save handling data from model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  before_save :translate
+
+  private
+
+  def translate
+    case job
+    when 'fman'
+      self.job = '낚시꾼'
+    when 'sman'
+      self.job = '창지기'
+    when 'lman'
+      self.job = '등대지기'
+    when 'wman'
+      self.job = '파도잡이'
+    when 'gman'
+      self.job = '길잡이'
+    end
+  end
 end
